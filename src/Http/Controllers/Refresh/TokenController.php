660202b1902b8b693cc06aafa46e5f1851ff6932
Jusdepixel <mandy@jusdepixel.fr>
2023-03-02 21:27:54 +0100

    feat(refresh): add routes for refresh my token an refresh my post from Instagram

--- src/Http/Controllers/Refresh/TokenController.php
+++ src/Http/Controllers/Refresh/TokenController.php
@@ -7,6 +7,7 @@ namespace Jusdepixel\InstagramApiLaravel\Http\Controllers\Refresh;
 use Exception;
 use Jusdepixel\InstagramApiLaravel\Instagram\Controller;
 use Illuminate\Http\Response;
+use Jusdepixel\InstagramApiLaravel\Models\InstagramUser;
 
 final class TokenController extends Controller
 {
@@ -18,6 +19,12 @@ final class TokenController extends Controller
         $profile = self::$instagram::setProfile([
             'token' => self::$instagram::requestRefreshToken()
         ]);
+
+        InstagramUser::query()
+            ->where('id', self::$instagram::getProfile()->userId)
+            ->update([
+                'access_token' => $profile->accessToken
+            ]);
 
         return response([
             'access_token' => $profile->accessToken
