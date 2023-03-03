47bba5d923b3ecc416a255716661f6a2406b70fa
Jusdepixel <mandy@jusdepixel.fr>
2023-03-03 08:53:29 +0100

    refactor: delete instagram_id, access_token..., token_type, expires_in, expires_in_human, add link to user

--- src/Http/Resources/User/UserResource.php
+++ src/Http/Resources/User/UserResource.php
@@ -19,28 +19,12 @@ class UserResource extends JsonResource
     {
         return [
             'id' => $this->id,
-            'instagram_id' => $this->instagram_id,
             'username' => $this->username,
             'media_count' => $this->media_count,
-            'access_token' => $this->access_token,
-            'token_type' => $this->token_type,
-            'expires_in' => $this->expires_in,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
-            'expires_in_human' => $this->expiresInHuman($this->expires_in),
+            'link' => '/api/users/' . $this->id,
             'posts' => new PostCollection($this->whenLoaded('posts')),
         ];
-    }
-
-    private function expiresInHuman(int $expiresIn): string
-    {
-        $startDate = Carbon::createFromTimestamp(time());
-        $endDate = Carbon::createFromTimestamp(time() + $expiresIn);
-
-        $days = $startDate->diffInDays($endDate);
-        $hours = $startDate->copy()->addDays($days)->diffInHours($endDate);
-        $minutes = $startDate->copy()->addDays($days)->addHours($hours)->diffInMinutes($endDate);
-
-        return "$days days, $hours hours and $minutes minutes";
     }
 }
