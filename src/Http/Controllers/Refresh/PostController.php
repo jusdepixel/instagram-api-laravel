2372d38b7419af37807df3cbae4d755d7af333c1
Jusdepixel <mandy@jusdepixel.fr>
2023-03-04 10:09:14 +0100

    fix(controller): refresh one post (author check)

--- src/Http/Controllers/Refresh/PostController.php
+++ src/Http/Controllers/Refresh/PostController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Jusdepixel\InstagramApiLaravel\Http\Controllers\Refresh;
 
 use Exception;
+use Jusdepixel\InstagramApiLaravel\Http\Resources\Post\PostResource;
 use Jusdepixel\InstagramApiLaravel\Instagram\Controller;
 use Illuminate\Http\Response;
 use Jusdepixel\InstagramApiLaravel\Models\InstagramPost;
@@ -14,20 +15,26 @@ final class PostController extends Controller
     /**
      * @throws Exception
      */
-    public function __invoke(string $instagramId): Response
+    public function __invoke(string $instagram_id): Response
     {
-        $url = self::$instagram::refreshMedia($instagramId);
-
-        InstagramPost::query()
-            ->update([
-                'media_url' => $url
+        try {
+            $post = InstagramPost::query()->where([
+                'instagram_id' => $instagram_id,
+                'instagram_user_id' => self::$instagram::getProfile()->user_id,
             ]);
 
-        return response([
-            'post' => InstagramPost::query()
-                ->with('author')
-                ->where('instagram_id', $instagramId)
-                ->first()
-        ]);
+            $post->update([
+                'media_url' => self::$instagram::refreshMedia($instagram_id)
+            ]);
+
+            return response([
+                'message' => 'Your post has been updated',
+                'post' => new PostResource($post->with('author')->first())
+            ]);
+        } catch (Exception) {
+            return response([
+                'message' => 'This post is not yours',
+            ], 400);
+        }
     }
 }
