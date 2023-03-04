ddc1122204e616055d6d86ebac65dad4f66dc50d
Jusdepixel <mandy@jusdepixel.fr>
2023-03-04 10:10:17 +0100

    refactor(controller): me delete post

--- src/Http/Controllers/Me/PostDeleteController.php
+++ src/Http/Controllers/Me/PostDeleteController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Jusdepixel\InstagramApiLaravel\Http\Controllers\Me;
 
+use Error;
 use Jusdepixel\InstagramApiLaravel\Models\InstagramPost;
 use Illuminate\Http\Response;
 
@@ -11,19 +12,13 @@ final class PostDeleteController
 {
     public function __invoke(string $id): Response
     {
-        $result = InstagramPost::query()->find($id);
-
-        if ($result) {
-            $result->delete();
-
+        try {
+            InstagramPost::query()->find($id)->delete();
+            return response(status: 204);
+        } catch (Error) {
             return response([
-                'message' => 'Post has been deleted'
-            ], 204);
-
+                'message' =>  'Post does not exist'
+            ], 400);
         }
-
-        return response([
-            'message' =>  'Post does not exist'
-        ], 400);
     }
 }
