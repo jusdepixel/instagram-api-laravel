8e84915d313845d61783ad47ca22a6d4d2c38863
Jusdepixel <mandy@jusdepixel.fr>
2023-03-04 10:06:41 +0100

    refactor(controller): post one

--- src/Http/Controllers/Posts/OneController.php
+++ src/Http/Controllers/Posts/OneController.php
@@ -4,26 +4,20 @@ declare(strict_types=1);
 
 namespace Jusdepixel\InstagramApiLaravel\Http\Controllers\Posts;
 
-use Jusdepixel\InstagramApiLaravel\Exceptions\InstagramException;
 use Jusdepixel\InstagramApiLaravel\Http\Resources\Post\PostResource;
 use Jusdepixel\InstagramApiLaravel\Models\InstagramPost;
-use Exception;
 use Illuminate\Http\Response;
 
 final class OneController
 {
     public function __invoke(string $id): Response|PostResource
     {
-        $post = new PostResource(
-            InstagramPost::query()->with('author')->find($id)
-        );
-
-        if ($post) {
-            return $post;
-        } else {
-            return (new InstagramException())->render(
-                new Exception("This post does not exist or no longer exists", 404)
-            );
+        if ($post = InstagramPost::query()->with('author')->find($id)) {
+            return new PostResource($post);
         }
+
+        return response([
+            'message' =>  'This post does not exist or no longer exists'
+        ], 404);
     }
 }
