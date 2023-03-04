04fddb495e477666481f8d14b62a5af1f346dc07
Jusdepixel <mandy@jusdepixel.fr>
2023-03-04 10:07:14 +0100

    fix(test): posts all

--- tests/Feature/Me/PostAllTest.php
+++ tests/Feature/Me/PostAllTest.php
@@ -6,6 +6,7 @@ namespace Jusdepixel\InstagramApiLaravel\Tests\Feature\Me;
 
 use Exception;
 use Illuminate\Testing\Fluent\AssertableJson;
+use Jusdepixel\InstagramApiLaravel\Database\Seeders\InstagramSeeder;
 use Jusdepixel\InstagramApiLaravel\Tests\Instagram;
 
 class PostAllTest extends Instagram
@@ -22,20 +23,21 @@ class PostAllTest extends Instagram
     public function test_response_success()
     {
         self::fakeProfile();
+        $this->seed(InstagramSeeder::class);
         $response = $this->get('/api/me/posts');
 
         $response->assertJson(fn (AssertableJson $json) =>
             $json
                 ->has('posts.data.0', fn ($json) =>
                     $json
-                        ->where('id', null)
+                        ->whereType('id', 'string')
                         ->where('instagram_id', 12345678910)
-                        ->where('instagram_user_id', '88888888-4444-4444-4444-121212121212')
+                        ->whereType('instagram_user_id', 'string')
                         ->where('caption', 'Caption Post !')
                         ->where('media_type', 'IMAGE')
                         ->where('media_url', 'http://media.url/12345678910')
                         ->where('permalink', 'https://perma.link/12345678910')
-                        ->where('username', 'userName')
+                        ->where('username', 'username')
                         ->where('timestamp', 1677267776)
                 )
         );
