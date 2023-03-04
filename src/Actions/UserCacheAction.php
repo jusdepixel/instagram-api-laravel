02a8b24bbb525a55c562e14f31fc5cc97dda525d
Jusdepixel <mandy@jusdepixel.fr>
2023-03-04 10:11:53 +0100

    refactor(action): user cache

--- src/Actions/UserCacheAction.php
+++ src/Actions/UserCacheAction.php
@@ -8,16 +8,11 @@ use Jusdepixel\InstagramApiLaravel\Instagram\Profile;
 use Jusdepixel\InstagramApiLaravel\Models\InstagramPost;
 use Illuminate\Support\Facades\Cache;
 
-/**
- * Remove all user datas
- * @package Jusdepixel\InstagramApiLaravel\Actions\UserCacheAction
- */
 final class UserCacheAction
 {
     public function __construct()
     {
-        $profile = new Profile();
-        $id = $profile::getProfile()->instagramId;
+        $id = (new Profile)::getProfile()->instagram_id;
 
         Cache::delete("my-posts-" . $id);
         Cache::delete("my-profile-" . $id);
@@ -27,8 +22,8 @@ final class UserCacheAction
             ->where('instagram_user_id', $id)
             ->get();
 
-        foreach ($postsUser as $postCacheToDelete) {
-            Cache::delete("post-" . $postCacheToDelete->instagram_id);
-        }
+        $postsUser->each(function ($post) {
+            Cache::delete("post-" . $post->instagram_id);
+        });
     }
 }
