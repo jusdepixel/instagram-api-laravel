5d30a23da32cad1b4948755991718d9fbe5e5cf3
Jusdepixel <mandy@jusdepixel.fr>
2023-03-05 21:21:44 +0100

    refactor(resources): me resource

--- src/Http/Resources/Me/MePostResource.php
+++ src/Http/Resources/Me/MePostResource.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Jusdepixel\InstagramApiLaravel\Http\Resources\Me;
 
+use Jusdepixel\InstagramApiLaravel\Instagram\Profile;
 use Jusdepixel\InstagramApiLaravel\Models\InstagramPost;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -14,13 +15,12 @@ class MePostResource extends JsonResource
     public function toArray($request): array
     {
         $post = InstagramPost::query()->select('id')->where('instagram_id', $this->id)->first();
-        $session = json_decode($request->getSession()->get('instagram'));
 
         return [
             'id' => $post?->id,
             'caption' => $this->caption,
             'instagram_id' => $this->id,
-            'instagram_user_id' => $session->instagram_user_id,
+            'instagram_user_id' => (new Profile)::getProfile()->instagram_user_id,
             'media_type' => $this->media_type,
             'media_url' => $this->media_url,
             'permalink' => $this->permalink,
