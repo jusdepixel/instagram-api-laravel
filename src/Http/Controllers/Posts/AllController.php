a0f6042e195ec1bea2a523b04909bb1cf14da1fa
Jusdepixel <mandy@jusdepixel.fr>
2023-03-23 11:52:32 +0100

    ci(posts): add cache all posts

--- src/Http/Controllers/Posts/AllController.php
+++ src/Http/Controllers/Posts/AllController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Jusdepixel\InstagramApiLaravel\Http\Controllers\Posts;
 
+use Illuminate\Support\Facades\Cache;
 use Jusdepixel\InstagramApiLaravel\Http\Resources\Post\PostCollection;
 use Jusdepixel\InstagramApiLaravel\Models\InstagramPost;
 
@@ -11,8 +12,12 @@ final class AllController
 {
     public function __invoke(): PostCollection
     {
-        return new PostCollection(
-             InstagramPost::with('instagram_user')->get()
-        );
+        if (!Cache::has('all-posts')) {
+            Cache::add('all-posts', new PostCollection(
+                InstagramPost::with('instagram_user')->get()
+            ));
+        }
+
+        return Cache::get('all-posts');
     }
 }
